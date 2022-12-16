92de534c5c44d807e4f95e5e204e355005f620f3
Bermejo246 <101293603+Bermejo246@users.noreply.github.com>
2022-12-15 21:38:07 -0600

    new changes

--- script.js
+++ script.js
@@ -1,35 +1,20 @@
 function init() {
+  
   var passLength = prompt('How long would you like the password?');
-    console.log(passLength);
-    
-    if(!passLength) return;
-    
-    if (passLength > 128){
-      alert("Error: Length is too long");
-        return init();
-      }  
 
-      if (passLength < 8){
-      alert("Error: Length is too short");
-      return init();
-    }
-
-    
     var temp = "";
     var password = "";
     var alpha = "abcdefghijklmnopqrstuvwxyz";
-    var numbers = confirm("Numbers in your password?");
     var lower = confirm("Lowercase letters in your password");
     var special = confirm("Special characters in your password?");
     var upper = confirm("Uppercase letters in your password?");    
     
     
-    
-    if (numbers) temp+="0123456789";
+    if(special) temp+="123456789!@#$%^&*()_+\|-=,./?";
     if (lower) temp+=alpha;
     if (upper) temp+=alpha.toUpperCase();
-    if(special) temp+="!@#$%^&*()_+-=,./?";
-    if(!lower && !numbers && !upper && !special) return;
+    
+    if(!lower && !upper && !numbers && !special) return;
     
     console.log(temp);
     
